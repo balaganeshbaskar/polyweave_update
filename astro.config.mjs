1d4420ec35d83be0e6574cebcd0c68a74a439aa8
balaganeshbaskar <balaganeshbaskar@gmail.com>
2026-05-15 19:24:50 +0530

    github pages ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://username.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
