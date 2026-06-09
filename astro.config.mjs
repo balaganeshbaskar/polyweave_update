22accaf8c785b50f4d0d694c36be1acc07d23844
balaganeshbaskar <balaganeshbaskar@gmail.com>
2026-06-09 16:35:16 +0530

    hosting update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // site: 'https://polyweave.net',
   site: 'https://balaganeshbaskar.github.io',
   base: '/polyweave_update',
   build: {
